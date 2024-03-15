dc4a4f90dece04be4a57efbf26e3f53e115bd104
Brooke Manning <brookemanning@brookes-air-4.devices.brown.edu>
2024-03-15 11:23:56 -0400

    Organization of loops

---
+++ gamestructure.m
@@ -0,0 +1,39 @@
+%GAME STRUCTURE
+
+%one function for the whole game 
+%function PingPong
+
+
+    %create gameboard 
+    
+    %initalize paddle positions
+    %initalize ball velocity, ball position
+    %initalize score keeping variables
+    %initialize arrow key states
+    
+    %set up keypress/keyrelease function
+    
+    
+    %create one while loop for the game
+    %WHILE LOOP
+    
+        %HANDLE BALL MOVMENT
+        %1) update ball position
+        %2) check collisions with left/right and top/bottom
+        %3)check collision with paddles
+        
+        %PADDLE MOVEMENT
+        %1) update paddleA position based on w,s
+        %2) update paddleB position based on up and down arrow
+        
+        %CHECK GAME CONDITION
+        %1) check score keep variables to see if the game should end
+        %2) break the while loop if the game is done
+    
+    
+    
+    %OUTSIDE THE WHILE LOOP
+    %callback function for key press
+    %callback function for key release
+
+
