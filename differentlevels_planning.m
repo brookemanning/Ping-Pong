5f9c6404f4525ed44ea9608b29b8b6eceb018225
Brooke Manning <brookemanning@brookes-air-4.devices.brown.edu>
2024-03-18 10:34:56 -0400

    adding levels

---
+++ differentlevels_planning.m
@@ -0,0 +1,16 @@
+%different levels
+
+%easy level
+%keep the same as most updated code
+%ball velocity = [0.015,0.015]
+%speedlevel = 0.03
+
+%medium level
+%change ball velocity
+%ballvelocity = [0.02, 0.02]
+%speedLevel = 0.03
+
+
+%hard level
+%ballvelocity = [0.03,0.03]
+%speedLevel = 0.04
